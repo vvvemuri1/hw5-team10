12e209a91af413b3159b1877efb2635e72c1d8fc
mabodx <mabodx@gmail.com>
2013-11-11 15:47:11 -0500

    finish

--- qa4mre-alzheimer-task/src/main/java/edu/cmu/lti/deiis/hw5/answer_ranking/AnswerScoreBaseClass.java
+++ qa4mre-alzheimer-task/src/main/java/edu/cmu/lti/deiis/hw5/answer_ranking/AnswerScoreBaseClass.java
@@ -13,10 +13,12 @@ import edu.cmu.lti.qalab.types.Answer;
 import edu.cmu.lti.qalab.types.CandidateAnswer;
 import edu.cmu.lti.qalab.types.CandidateSentence;
 import edu.cmu.lti.qalab.types.CandidateSentenceAnswerSet;
+import edu.cmu.lti.qalab.types.Dependency;
 import edu.cmu.lti.qalab.types.NER;
 import edu.cmu.lti.qalab.types.NounPhrase;
 import edu.cmu.lti.qalab.types.Question;
 import edu.cmu.lti.qalab.types.QuestionAnswerSet;
+import edu.cmu.lti.qalab.types.Synonym;
 import edu.cmu.lti.qalab.types.TestDocument;
 import edu.cmu.lti.qalab.utils.Utils;
 
@@ -75,43 +77,59 @@ public class AnswerScoreBaseClass extends JCasAnnotator_ImplBase {
 				sentence.getSentence().getPhraseList(), NounPhrase.class);
 		ArrayList<NER> candSentNers = Utils.fromFSListToCollection(sentence
 				.getSentence().getNerList(), NER.class);
+		ArrayList<Dependency> candSentDependencies = Utils.fromFSListToCollection(sentence.getSentence().getDependencyList(), Dependency.class);
+		
 		ArrayList<NounPhrase> choiceNouns = Utils.fromFSListToCollection(
 				answer.getNounPhraseList(), NounPhrase.class);
-		ArrayList<NER> choiceNERs = Utils.fromFSListToCollection(
+		ArrayList<NER> choiceNers = Utils.fromFSListToCollection(
 				answer.getNerList(), NER.class);
-
-		int nnMatch = 0;
+		ArrayList<Dependency> choiceDependencies = Utils.fromFSListToCollection(sentence.getSentence().getDependencyList(), Dependency.class);
+		
+		
+		
+		double nnNounPhrase=0, nnNER=0, nnDependency=0;
+		double scoreNounPhrase=0, scoreNER=0, scoreDependency=0;
+		
 		for (int k = 0; k < candSentNouns.size(); k++) {
-			for (int l = 0; l < choiceNERs.size(); l++) {
-				if (candSentNouns.get(k).getText()
-						.contains(choiceNERs.get(l).getText())) {
-					nnMatch++;
-				}
-			}
 			for (int l = 0; l < choiceNouns.size(); l++) {
 				if (candSentNouns.get(k).getText()
-						.contains(choiceNouns.get(l).getText())) {
-					nnMatch++;
+						.equals(choiceNouns.get(l).getText())) {
+					nnNounPhrase+=1;
 				}
 			}
 		}
+		
+		if(candSentNouns.size()+choiceNouns.size()!=0 )
+			scoreNounPhrase=nnNounPhrase/((double)candSentNouns.size()+ choiceNouns.size());
+		
 
 		for (int k = 0; k < candSentNers.size(); k++) {
-			for (int l = 0; l < choiceNERs.size(); l++) {
-				if (candSentNouns.get(k).getText()
-						.contains(choiceNERs.get(l).getText())) {
-					nnMatch++;
+			for (int l = 0; l < choiceNers.size(); l++) {
+				if (candSentNers.get(k).getText()
+						.equals(choiceNers.get(l).getText())) {
+					nnNER+=1;
 				}
 			}
-			for (int l = 0; l < choiceNouns.size(); l++) {
-				if (candSentNouns.get(k).getText()
-						.contains(choiceNouns.get(l).getText())) {
-					nnMatch++;
-				}
-			}
-
 		}
-		return (double) nnMatch;
+		if(candSentNers.size()+choiceNers.size()!=0 )
+			scoreNER = nnNER/((double)candSentNers.size() + choiceNers.size());
+		
+		
+		for (int k = 0; k < candSentDependencies.size(); k++) {
+			for (int l = 0; l < choiceDependencies.size(); l++) {
+				if (candSentDependencies.get(k).getCoveredText()
+						.equals(choiceDependencies.get(l).getCoveredText())) {
+					nnDependency+=1;
+				}
+			}
+		}
+		
+		if(candSentDependencies.size()+choiceDependencies.size()!=0 )
+			scoreDependency = nnDependency/((double)candSentDependencies.size() + choiceDependencies.size());
+		
+		double ans = (scoreDependency + scoreNER + scoreNounPhrase)/3;
+//		System.out.println("***" + ans +"**");
+		return ans;
 	}
   
   
